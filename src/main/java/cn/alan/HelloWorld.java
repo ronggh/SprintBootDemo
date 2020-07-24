4b60c817f55c02fa6287f3eb789a402e1bc73add
ronggh@sina.cn <ronggh@sina.cn>
2020-07-24 13:05:48 +0800

    init

--- src/main/java/cn/alan/HelloWorld.java
+++ src/main/java/cn/alan/HelloWorld.java
@@ -3,6 +3,7 @@ package cn.alan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Spring boot application
 @SpringBootApplication
 public class HelloWorld {
     public static void main(String[] args) {
